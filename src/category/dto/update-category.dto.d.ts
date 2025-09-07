cfaeff4810bb0b01eda3aa7788b2a9f275dc111f
Mao <vmaithalee@gmail.com>
2025-09-07 14:24:52 +0530

    Changes added

--- src/category/dto/update-category.dto.d.ts
+++ src/category/dto/update-category.dto.d.ts
@@ -2,4 +2,4 @@ import { CreateCategoryDto } from './create-category.dto';
 declare const UpdateCategoryDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateCategoryDto>>;
 export declare class UpdateCategoryDto extends UpdateCategoryDto_base {
 }
-export {};
+export export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {};
--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,14 +1,17 @@
 import { DataSource } from "typeorm";
 import { Category } from "./category/entities/category.entity";
+import * as dotenv from 'dotenv';
+import process from "process";
+dotenv.config();
 
 export const appSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres', 
-  password: '76543210', 
-  database: 'ecommerce_db',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
+  username: process.env.DB_USERNAME || 'postgres', 
+  password: process.env.DB_PASSWORD || '76543210', 
+  database: process.env.DB_NAME ||'ecommerce_db',
   entities: [Category],
   migrations: ['src/migrations/*.ts'],
-  synchronize: false
-})
+  synchronize: (process.env.TYPEORM_SYNCHRONIZE === 'true' ) || false
+});
